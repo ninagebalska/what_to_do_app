fdd9ae5446a6c8dfe78c8a420cf5b3bece0e572f
Nina Gębalska <ninagebalska@gmail.com>
2023-04-28 21:13:33 +0200

    Create HomePage with appBar and action button

--- lib/features/home/cubit/home_cubit.dart
+++ lib/features/home/cubit/home_cubit.dart
@@ -4,5 +4,5 @@ import 'package:meta/meta.dart';
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeInitial());
+  HomeCubit() : super(HomeState());
 }
--- lib/features/home/page/home_page.dart
+++ lib/features/home/page/home_page.dart
@@ -1,10 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color(0xFFF6FFF8),
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'Your Daily Tasks',
+          style: GoogleFonts.poppins(
+            color: const Color(0xFF354F52),
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      body: ListView(children: [
+        _EventTile('zadanie 1'),
+        _EventTile('zadanie 2'),
+        _EventTile('zadanie 3'),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color(0xFFFDA769),
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          color: Color(0xFFF6FFF8),
+        ),
+      ),
+    );
+  }
+}
+
+class _EventTile extends StatelessWidget {
+  const _EventTile(this.title,{
+    Key? key, 
+  }) : super(key: key);
+
+  final String title;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color(0xFF52796F),
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 6,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Text(
+        title,
+        style: GoogleFonts.poppins(
+            color: const Color(0xFFFBFBFB),
+            fontWeight: FontWeight.bold,
+            fontSize: 16),
+      ),
+    );
   }
 }
