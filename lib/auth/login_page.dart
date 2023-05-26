ac018855c95f3d8fe952ffca3887c8f2d211a5d1
Nina Gębalska <ninagebalska@gmail.com>
2023-05-26 21:25:01 +0200

    Add cubit to login/register page

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -13,94 +13,99 @@ class LoginPage extends StatelessWidget {
       backgroundColor: const Color(0xFFFBFBFB),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Image(image: AssetImage('images/login.png')),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  'Login',
-                  style: GoogleFonts.poppins(
-                    fontSize: 36,
-                    color: const Color(0xFF4158D0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Image(
+                image: AssetImage('images/login1.png'),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Login',
+                    style: GoogleFonts.poppins(
+                      fontSize: 36,
+                      color: const Color(0xFF4158D0),
+                    ),
+                  ),
+                  Text(
+                    'Please sign in to continue.',
+                    style: GoogleFonts.poppins(
+                      fontSize: 15,
+                      color: const Color(0xFF4158D0),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 36),
+              _ThemedTextField(
+                controller: emailController,
+                hintText: 'example@gmail.com',
+                icon: const Icon(
+                  Icons.email_rounded,
+                  color: Colors.orange,
+                ),
+                obscureText: false,
+              ),
+              const SizedBox(height: 18),
+              _ThemedTextField(
+                controller: passwordController,
+                hintText: 'password',
+                icon: const Icon(
+                  Icons.lock_rounded,
+                  color: Colors.orange,
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 18),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(55),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32.0),
                   ),
                 ),
-                Text(
-                  'Please sign in to continue.',
-                  style: GoogleFonts.poppins(
-                    fontSize: 15,
-                    color: const Color(0xFF4158D0),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 36),
-            _ThemedTextField(
-              controller: emailController,
-              hintText: 'example@gmail.com',
-              icon: const Icon(
-                Icons.email_rounded,
-                color: Colors.orange,
-              ),
-              obscureText: false,
-            ),
-            const SizedBox(height: 18),
-            _ThemedTextField(
-              controller: passwordController,
-              hintText: 'password',
-              icon: const Icon(
-                Icons.lock_rounded,
-                color: Colors.orange,
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 18),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size.fromHeight(55),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(32.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'LOGIN',
+                      style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Icon(
+                      Icons.arrow_right_alt_rounded,
+                      color: Colors.white,
+                    )
+                  ],
                 ),
               ),
-              child: Row(
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    'LOGIN',
-                    style: GoogleFonts.poppins(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
+                    'Don\'t have an account?',
+                    style: GoogleFonts.poppins(color: Colors.black54),
                   ),
-                  const Icon(
-                    Icons.arrow_right_alt_rounded,
-                    color: Colors.white,
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        foregroundColor: const Color(0xFF4158D0)),
+                    child: Text(
+                      'Register',
+                      style: GoogleFonts.poppins(),
+                    ),
                   )
                 ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Don\'t have an account?',
-                  style: GoogleFonts.poppins(color: Colors.black54),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                      foregroundColor: const Color(0xFF4158D0)),
-                  child: const Text(
-                    'Register',
-                  ),
-                )
-              ],
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
