7836f2b029380610af3600e24a1db316de6a3688
Nina Gębalska <ninagebalska@gmail.com>
2023-05-28 22:17:04 +0200

    Add bloc logic to drawer

--- lib/features/drawer/drawer.dart
+++ lib/features/drawer/drawer.dart
@@ -1,108 +1,131 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:what_to_do_app/auth/cubit/auth_cubit.dart';
+import 'package:what_to_do_app/auth/login_page.dart';
 import 'package:what_to_do_app/auth/user_profile.dart';
+import 'package:what_to_do_app/domain/repositories/auth_repository.dart';
+import 'package:what_to_do_app/enums.dart';
 
 class NavigationDrawer extends StatelessWidget {
   const NavigationDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Container(
-          width: 250,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF4158D0),
-                Color(0xFFC850C0),
-                Color(0xFFFFCC70),
-              ],
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 72.0),
-            child: Column(
-              children: [
-                Container(
-                  width: 180,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: const Color(0xFF6200EE),
-                      width: 5.0,
-                    ),
-                  ),
-                  child: const CircleAvatar(
-                    radius: 100,
-                    backgroundImage: NetworkImage(
-                        'https://thumbs.dreamstime.com/b/purple-user-icon-circle-shadow-solid-gradient-thin-linear-sign-136856587.jpg'),
-                    backgroundColor: Colors.white,
+    return BlocProvider(
+      create: (context) => AuthCubit(AuthRepository()),
+      child: BlocConsumer<AuthCubit, AuthState>(
+        listener: (context, state) {
+          if (state.status == Status.success) {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => LoginPage(),
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          return Drawer(
+            child: Container(
+                width: 250,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xFF4158D0),
+                      Color(0xFFC850C0),
+                      Color(0xFFFFCC70),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 20),
-                Text(
-                  'Hello!',
-                  style: GoogleFonts.poppins(
-                    fontSize: 36,
-                    color: const Color(0xFFfbfbfb),
-                  ),
-                ),
-                const SizedBox(height: 60),
-                ListTile(
-                  leading: const Icon(
-                    Icons.home_rounded,
-                    color: Color(0xFFfbfbfb),
-                  ),
-                  title: Text(
-                    'HOME',
-                    style: GoogleFonts.poppins(
-                      fontSize: 18,
-                      color: const Color(0xFFfbfbfb),
-                    ),
-                  ),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(
-                    Icons.person_rounded,
-                    color: Color(0xFFfbfbfb),
-                  ),
-                  title: Text(
-                    'PROFILE',
-                    style: GoogleFonts.poppins(
-                      fontSize: 18,
-                      color: const Color(0xFFfbfbfb),
-                    ),
-                  ),
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const UserProfile(),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 72.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 180,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: const Color(0xFF6200EE),
+                            width: 5.0,
+                          ),
+                        ),
+                        child: const CircleAvatar(
+                          radius: 100,
+                          backgroundImage: NetworkImage(
+                              'https://thumbs.dreamstime.com/b/purple-user-icon-circle-shadow-solid-gradient-thin-linear-sign-136856587.jpg'),
+                          backgroundColor: Colors.white,
+                        ),
                       ),
-                    );
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(
-                    Icons.logout_rounded,
-                    color: Color(0xFFfbfbfb),
+                      const SizedBox(height: 20),
+                      Text(
+                        'Hello!',
+                        style: GoogleFonts.poppins(
+                          fontSize: 36,
+                          color: const Color(0xFFfbfbfb),
+                        ),
+                      ),
+                      const SizedBox(height: 60),
+                      ListTile(
+                        leading: const Icon(
+                          Icons.home_rounded,
+                          color: Color(0xFFfbfbfb),
+                        ),
+                        title: Text(
+                          'HOME',
+                          style: GoogleFonts.poppins(
+                            fontSize: 18,
+                            color: const Color(0xFFfbfbfb),
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(
+                          Icons.person_rounded,
+                          color: Color(0xFFfbfbfb),
+                        ),
+                        title: Text(
+                          'PROFILE',
+                          style: GoogleFonts.poppins(
+                            fontSize: 18,
+                            color: const Color(0xFFfbfbfb),
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => const UserProfile(),
+                            ),
+                          );
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(
+                          Icons.logout_rounded,
+                          color: Color(0xFFfbfbfb),
+                        ),
+                        title: Text(
+                          'LOG OUT',
+                          style: GoogleFonts.poppins(
+                            fontSize: 18,
+                            color: const Color(0xFFfbfbfb),
+                          ),
+                        ),
+                        onTap: () {
+                          context.read<AuthCubit>().signOut();
+                        },
+                      ),
+                    ],
                   ),
-                  title: Text(
-                    'LOG OUT',
-                    style: GoogleFonts.poppins(
-                      fontSize: 18,
-                      color: const Color(0xFFfbfbfb),
-                    ),
-                  ),
-                  onTap: () {},
-                ),
-              ],
-            ),
-          )),
+                )),
+          );
+        },
+      ),
     );
   }
 }
--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -7,57 +7,95 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(40.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.network(
-                  'https://cdni.iconscout.com/illustration/free/thumb/free-task-list-2080780-1753768.png'),
-              const SizedBox(height: 50),
-              Text(
-                'Get your things done with TODO',
-                style: GoogleFonts.poppins(
-                    color: const Color(0xFF4158D0),
-                    fontSize: 19,
-                    fontWeight: FontWeight.w600),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF4158D0),
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: size.height * 0.35,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF4158D0),
+                  Color(0xFFC850C0),
+                  Color(0xFFFFCC70),
+                ],
               ),
-              const SizedBox(height: 25),
-              Text(
-                'Manage your daily tasks, stay organized and get things done efficiently',
-                style: GoogleFonts.poppins(
-                  fontSize: 15,
-                  color: const Color(0xFF7584D0),
-                ),
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(70),
+                  bottomRight: Radius.circular(70)),
+            ),
+            child: const Align(
+              alignment: Alignment(0, -1),
+              child: Image(
+                image: AssetImage('images/welcome_image.png'),
+                height: 240,
               ),
-              const SizedBox(height: 50),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => LoginPage(),
-                    ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(40),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(32.0),
-                  ),
-                ),
-                child: Text(
-                  'Start now!',
-                  style: GoogleFonts.poppins(
-                    color: Colors.white,
-                    fontSize: 28,
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+          const SizedBox(height: 20),
+          Container(
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              children: [
+                Text(
+                  'Get your things done with TaskTracker',
+                  style: GoogleFonts.poppins(
+                      color: const Color(0xFF4158D0),
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(height: 25),
+                Text(
+                  'Manage your daily tasks, stay organized and get things done efficiently!',
+                  style: GoogleFonts.poppins(
+                    fontSize: 15,
+                    color: const Color(0xFF7584D0),
+                  ),
+                ),
+                const SizedBox(height: 50),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => LoginPage(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(55),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32.0),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'START NOW',
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Icon(
+                        Icons.arrow_right_alt_rounded,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
